c2facff83049745756aa7417e1135400afcf9f37
azoz478 <azoz.majed.331@gmail.com>
2026-02-09 17:59:45 +0300

    Update fast.lua

--- fast.lua
+++ fast.lua
@@ -1,5 +1,36 @@
-while true do
-    task.wait(20) -- تم التعديل لـ 20 ثانية
-    local ts = game:GetService("TeleportService")
-    ts:Teleport(game.PlaceId, game.Players.LocalPlayer)
-end
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+-- Creating the Main Window
+local Window = OrionLib:MakeWindow({
+    Name = "Server Management", 
+    HidePremium = false, 
+    SaveConfig = true, 
+    ConfigFolder = "OrionSettings"
+})
+
+-- Adding a Tab
+local MainTab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+-- Adding the Server Hop Button
+MainTab:AddButton({
+	Name = "Server Hop (20 Seconds)",
+	Callback = function()
+        -- Notification for the user
+        OrionLib:MakeNotification({
+            Name = "System Message",
+            Content = "Teleporting to a new server in 20 seconds...",
+            Image = "rbxassetid://4483345998",
+            Time = 5
+        })
+        
+        task.wait(20)
+        local ts = game:GetService("TeleportService")
+        ts:Teleport(game.PlaceId, game.Players.LocalPlayer)
+  	end    
+})
+
+OrionLib:Init()
