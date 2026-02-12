d164f9d9edf691f6d71e00f1de2e17a92b09e9ef
azoz478 <azoz.majed.331@gmail.com>
2026-02-12 15:17:21 +0300

    Implement tweening for character movement
    Replaced direct position setting with tweening to a new goal position.

--- fast.lua
+++ fast.lua
@@ -1,4 +1,13 @@
+local ts = game:GetService("TweenService")
 local root = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
 
--- هذا السطر ينقلك فوراً للهدف
-root.CFrame = CFrame.new(913, -2.99, -2.66)
+-- الوجهة (حط إحداثياتك الجديدة هنا)
+local goal = {CFrame = CFrame.new(905.471802, -11.2686329, -585.484741, 1, 0, 0, 0, 1, 0, 0, 0, 1}
+
+-- السرعة: هنا السرعة الخارقة (نص ثانية فقط!)
+local info = TweenInfo.new(0.5, Enum.EasingStyle.Linear)
+
+local flyTween = ts:Create(root, info, goal)
+flyTween:Play()
+
+print("انطلقناااا!")
