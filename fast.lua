118ca6d662860c59d02946da3e6d2efe3911eb6c
azoz478 <azoz.majed.331@gmail.com>
2026-02-12 15:15:38 +0300

    Teleport player to specified coordinates
    Replaced tween animation with direct teleportation to target coordinates.

--- fast.lua
+++ fast.lua
@@ -1,13 +1,4 @@
-local ts = game:GetService("TweenService")
 local root = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
 
--- الوجهة (حط إحداثياتك الجديدة هنا)
-local goal = {CFrame = CFrame.new(913, 10, -2.6)}
-
--- السرعة: هنا السرعة الخارقة (نص ثانية فقط!)
-local info = TweenInfo.new(0.5, Enum.EasingStyle.Linear)
-
-local flyTween = ts:Create(root, info, goal)
-flyTween:Play()
-
-print("انطلقناااا!")
+-- هذا السطر ينقلك فوراً للهدف
+root.CFrame = CFrame.new(913, -2.99, -2.66)
