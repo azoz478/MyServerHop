3ebeb15783b6e06c890f78f8edd741a38382660f
azoz478 <azoz.majed.331@gmail.com>
2026-02-12 14:47:50 +0300

    Update fast.lua

--- fast.lua
+++ fast.lua
@@ -1,1 +1,7 @@
-local target = {CFrame = CFrame.new(913, -2.99998093, -2.66858042e-05, 1, 0, 0, 0, 1, 0, 0, 0, 1)}
+local ts = game:GetService("TweenService")
+local char = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
+local root = char:WaitForChild("HumanoidRootPart")
+local target = {CFrame = CFrame.new(913, -2.99, -2.66, 1, 0, 0, 0, 1, 0, 0, 0, 1)}
+local info = TweenInfo.new(10, Enum.EasingStyle.Linear)
+local tween = ts:Create(root, info, target)
+tween:Play()
