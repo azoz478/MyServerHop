bdaf6f493f1cb0cdaa4c8aaffa5701c0769fa466
azoz478 <azoz.majed.331@gmail.com>
2026-02-12 14:51:12 +0300

    Update fast.lua

--- fast.lua
+++ fast.lua
@@ -2,6 +2,9 @@ local ts = game:GetService("TweenService")
 local char = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
 local root = char:WaitForChild("HumanoidRootPart")
 local target = {CFrame = CFrame.new(913, -2.99, -2.66, 1, 0, 0, 0, 1, 0, 0, 0, 1)}
-local info = TweenInfo.new(10, Enum.EasingStyle.Linear)
+
+-- الرقم 1 هنا يعني السرعة قصوى (يصل في ثانية واحدة)
+local info = TweenInfo.new(20, Enum.EasingStyle.Linear)
+
 local tween = ts:Create(root, info, target)
 tween:Play()
