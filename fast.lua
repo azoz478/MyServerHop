03ad678c0fdae0fdee901d14984cbbe5a6ddbe1e
azoz478 <azoz.majed.331@gmail.com>
2026-02-12 15:19:39 +0300

    Refactor tweening to use character and increase duration
    Updated tweening logic to use character reference and adjusted duration.

--- fast.lua
+++ fast.lua
@@ -1,13 +1,7 @@
 local ts = game:GetService("TweenService")
-local root = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
-
--- الوجهة (حط إحداثياتك الجديدة هنا)
-local goal = {CFrame = CFrame.new(905.471802, -11.2686329, -585.484741, 1, 0, 0, 0, 1, 0, 0, 0, 1}
-
--- السرعة: هنا السرعة الخارقة (نص ثانية فقط!)
-local info = TweenInfo.new(0.5, Enum.EasingStyle.Linear)
-
-local flyTween = ts:Create(root, info, goal)
-flyTween:Play()
-
-print("انطلقناااا!")
+local char = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
+local root = char:WaitForChild("HumanoidRootPart")
+local target = {CFrame = CFrame.new(905.471802, -11.2686329, -585.484741, 1, 0, 0, 0, 1, 0, 0, 0, 1)}
+local info = TweenInfo.new(100, Enum.EasingStyle.Linear)
+local tween = ts:Create(root, info, target)
+tween:Play()
