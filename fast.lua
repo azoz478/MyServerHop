15c4b92efb71510184ae6b7487cab3b4aa64bb17
azoz478 <azoz.majed.331@gmail.com>
2026-02-12 15:12:26 +0300

    Update fast.lua

--- fast.lua
+++ fast.lua
@@ -1,13 +1,4 @@
-local ts = game:GetService("TweenService")
 local root = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
 
--- الوجهة (حط إحداثياتك الجديدة هنا)
-local goal = {CFrame = CFrame.new(905.471802, -11.2686329, -585.484741, 1, 0, 0, 0, 1, 0, 0, 0, 1)}
-
--- السرعة: هنا السرعة الخارقة (نص ثانية فقط!)
-local info = TweenInfo.new(0.5, Enum.EasingStyle.Linear)
-
-local flyTween = ts:Create(root, info, goal)
-flyTween:Play()
-
-print("انطلقناااا!")
+-- هذا السطر ينقلك فوراً للهدف
+root.CFrame = CFrame.new(905.471802, -11.2686329, -585.484741, 1, 0, 0, 0, 1, 0, 0, 0, 1)
