fccbab4a092ab528ff01d4e40bdfc8be806a2808
azoz478 <azoz.majed.331@gmail.com>
2026-02-12 15:21:51 +0300

    Reduce tween duration from 100s to 0.5s

--- fast.lua
+++ fast.lua
@@ -2,6 +2,6 @@ local ts = game:GetService("TweenService")
 local char = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
 local root = char:WaitForChild("HumanoidRootPart")
 local target = {CFrame = CFrame.new(905.471802, -11.2686329, -585.484741, 1, 0, 0, 0, 1, 0, 0, 0, 1)}
-local info = TweenInfo.new(100, Enum.EasingStyle.Linear)
+local info = TweenInfo.new(0.5, Enum.EasingStyle.Linear)
 local tween = ts:Create(root, info, target)
 tween:Play()
